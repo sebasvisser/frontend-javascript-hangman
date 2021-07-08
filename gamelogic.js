c05e603b6ee5431774554bbec757c8431ce7496a
sebasvisser <sebas.visser@mensenrelatie.nl>
2021-07-08 20:09:11 +0200

    gameIsWon opgelost op lange manier

--- gamelogic.js
+++ gamelogic.js
@@ -1,13 +1,47 @@
 function displayWordSoFar(word, guesses) {
+// aanmaken variabele om te verzamelen
   let displayedWord = "";
-  for (let i = 0; i < word.length; i++) {
-    displayedWord =displayedWord + ("_ ");
+// word argument omzetten naar array om te kunnen doorlopen
+  const splitWord = word.split("");
+// array doorlopen in array
+  for (let i = 0; i < splitWord.length; i++) {
+// als in de geraden letters de huidige letter zit, dan...
+    if (guesses.includes(splitWord[i])){
+// huidige letter stoppen in verzamelvariabele met extra spatie
+      displayedWord = displayedWord + splitWord[i] + " ";
+//als in guesses geen letter zit die overeenkomt met huidige letter
+    } else {
+// dan een underscore laten zien
+      displayedWord = displayedWord + ("_ ");
+    }
   }
+// verzamel variabele teruggeven
 return displayedWord;
 }
 
 function isGameWon(word, guesses) {
-  // WRITE ME
+// aanmaken variabele om te verzamelen
+  let displayedWord = "";
+// word argument omzetten naar array om te kunnen doorlopen
+  const splitWord = word.split("");
+// array doorlopen in array
+  for (let i = 0; i < splitWord.length; i++) {
+// als in de geraden letters de huidige letter zit, dan...
+    if (guesses.includes(splitWord[i])){
+// huidige letter stoppen in verzamelvariabele ZONDER spatie
+      displayedWord = displayedWord + splitWord[i];
+//als in guesses geen letter zit die overeenkomt met huidige letter
+    } else {
+// dan een underscore laten zien
+      displayedWord = displayedWord + ("_ ");
+    }
+  }
+// als verzamelvariabele gelijk is aan argument word, dan return true
+  if (displayedWord == word) {
+  return true;
+  } else {
+  return false;
+  }
 }
 
 function isGameLost(word, guesses) {
