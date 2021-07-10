82e89c6f056b0adaed913a372a17abb5ade6034e
sebasvisser <sebas.visser@mensenrelatie.nl>
2021-07-10 23:44:38 +0200

    Nog nettere verwerking van teveel tekens. + Code Style

--- index.js
+++ index.js
@@ -1,68 +1,53 @@
 const { question } = require("readline-sync");
 const { displayWordSoFar, isGameWon, isGameLost } = require("./gamelogic");
 
-console.clear(); // beginnen met een schone terminal
+// beginnen met een schone terminal
+console.clear();
 
 function game(word, guesses) {
   if (guesses.length !== 0) {
-      const guessesSoFar = guesses.toString();
+    const guessesSoFar = guesses.toString();
 //guesses als string displayen omdat anders niet mooi in terminal (multiple lines)
     console.log("Deze letters heb je al geprobeerd: ", guessesSoFar);
   }
 
   const letter = question("Raad een letter: ");
-  // voorkomen van zelfde letter nog eens raden.
+// voorkomen van zelfde letter nog eens raden.
   if(guesses.includes(letter)){
-      console.log("Die letter had je al.")
-      game(word, guesses);
+    console.log("Die letter had je al geprobeerd.")
+    game(word, guesses);
   }
-  //voorkomen gekke tekens
-    const searchLetter= letter.toString();
-    if(!"abcdefghijklmnopqrstuvwxyz".includes(searchLetter)) {
-        console.log("Dat is geen normale letter.")
-        game(word, guesses);
-    }
-// controle of input 1 character lang is => anders boos worden
-  if(letter.length === 1) {
-// voeg de geraden letter toe aan de array met guesses,
-// waarbij letter is omgezet naar lowercase.
+//voorkomen gekke tekens
+  const searchLetter= letter.toString();
+  if(!"abcdefghijklmnopqrstuvwxyz".includes(searchLetter)) {
+    console.log("Dat is geen normale letter.")
+    game(word, guesses);
+  }
+// controle of input 1 character lang is
+  if(!letter.length === 1) {
+    console.log("Zou het lukken om je te beperken tot 1 letter?.")
+    game(word, guesses);
+  }
+// Letter toevoegen aan guesses (als lowercase)
   guesses.push(letter.toLowerCase());
-    } else { //  indien meerdere letters ingevoerd, of niet alfabet afstraffen! ;P
-        const failGraphic = "   _______________                        |*\\_/*|________\n" +
-        "  |  ___________  |     .-.     .-.      ||_/-\\_|______  |\n" +
-        "  | |           | |    .****. .****.     | |           | |\n" +
-        "  | |   0   0   | |    .*****.*****.     | |   0   0   | |\n" +
-        "  | |     -     | |     .*********.      | |     -     | |\n" +
-        "  | |   \\___/   | |      .*******.       | |   \\___/   | |\n" +
-        "  | |___     ___| |       .*****.        | |___________| |\n" +
-        "  |_____|\\_/|_____|        .***.         |_______________|\n" +
-        "    _|__|/ \\|_|_.............*.............._|________|_\n" +
-        "   / ********** \\                          / ********** \\\n" +
-        " /  ************  \\                      /  ************  \\\n" +
-        "--------------------                    --------------------";
-        console.clear(); // lege terminal bij deze faal
-        console.log(failGraphic);
-        console.log(" You had one task..1 letter invoeren. Hoe moeilijk kan het zijn?");
-        console.log(" Doei doei. We beginnen voor straf HELEMAAL OPNIEUW!");
-        game(word, []);
-      }
-
-  console.clear(); // elke poging met lege terminal beginnen
-    const attempts = guesses.length + 1; //pogingen tellen
-    console.log(" ========== Poging " + attempts +  " ==========")
+// Aantal pogingen tellen
+  const attempts = guesses.length + 1;
+// elke poging met lege terminal beginnen
+  console.clear();
+  console.log(" ========== Poging " + attempts +  " ==========")
 // laat zien hoe het woord er tot nu toe uitziet
-    console.log("Het te raden woord: " + displayWordSoFar(word, guesses));
+  console.log("Het te raden woord: " + displayWordSoFar(word, guesses));
 
-// als gewonnen => display gewonnen
-// verloren => display verloren
-// anders game nog een keer aanroepen
   if(isGameWon(word, guesses)) {
+// als gewonnen => display gewonnen
     console.log("Je hebt gewonnen!");
   } else if(isGameLost(word, guesses)) {
+// verloren => display verloren
     console.log("Te vaak een verkeerde letter geraden... Je hebt verloren, jammer!");
   } else  {
-        game(word, guesses);
-    }
+// anders game nog een keer aanroepen
+    game(word, guesses);
+  }
 }
 
 console.log(`
